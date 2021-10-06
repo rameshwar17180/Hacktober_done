43e80588f244e7b223fc4761da8ce70dcb94cdc7
rameshwar17180 <43911196+rameshwar17180@users.noreply.github.com>
2021-10-06 10:12:11 +0530

    Update test.java

--- test.java
+++ test.java
@@ -18,10 +18,28 @@ public class test {
 	    		arr[i][j]=Integer.parseInt(st[j]);
 	    		pos[i][j]=0;
 	    	}
+		    if (left[l] <= right[h]) {
+                arr[k] = left[l];
+                k++;
+                l++;
+            } else {
+                arr[k] = right[h];
+                k++;
+                h++;
+            }
 	    }
+		test.bubblesort(arr);
+		
 	    int count=0;
 	    for(int i=0;i<n;i++) {
 	    	for(int j=0;j<m;j++) {
+			while (j >= 0) {
+                if (arr[j] > key) {
+                    arr[j + 1] = arr[j];
+                    j--;
+                } else
+                    break;
+            }
 	    		arr[i][j]^=pos[i][j];
 	    		pos[i][j+1]^=pos[i][j];
 	    		pos[i+1][j+1]^=pos[i][j];
@@ -31,19 +49,35 @@ public class test {
 	    			pos[i][j]^=1;
 	    			pos[i+1][j]^=1;
 	    			pos[i][j+1]^=1;
-	    			pos[i+1][j+1]^=1;
-	    			pos[i+k][j]^=1;
-	    			pos[i][j+k]^=1;
-	    			pos[i+k][j+k]^=1;
-	    			count+=1;
+	    			
 	    		}
 	    		else if(arr[i][j]==0){
 	    			count=-1;
 	    			System.out.println(count);
+				pos[i+1][j+1]^=1;
+	    			pos[i+k][j]^=1;
+	    			pos[i][j+k]^=1;
+	    			pos[i+k][j+k]^=1;
+	    			count+=1;
 	    			return;
 	    		}
 	    	}
 	    }
 	    System.out.println(count);
 	}
+	public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j + 1);
+                }
+            }
+        }
+		public static void swap(int[] arr, int lo, int hi) {
+        int temp = arr[lo];
+        arr[lo] = arr[hi];
+        arr[hi] = temp;
+    }
+    }
 }
