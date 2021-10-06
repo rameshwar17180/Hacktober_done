4e644fab7fb1bf334cef68bee0e5b97084e53237
rameshwar17180 <43911196+rameshwar17180@users.noreply.github.com>
2021-10-06 10:13:44 +0530

    added quicksort

--- Sortingfiles.java
+++ Sortingfiles.java
@@ -13,3 +13,32 @@ public static void insertionSort(int[] arr) {
             arr[j + 1] = key;
         }
     }
+ public static void quickSort(int[] arr) {
+        quickSortUtil(arr, 0, arr.length - 1);
+    }
+
+    public static void quickSortUtil(int[] arr, int lo, int hi) {
+        if (lo >= hi)
+            return;
+        int partition = partitionUtil(arr, lo, hi);
+        if (partition - 1 > lo)
+            quickSortUtil(arr, lo, partition - 1);
+        if (partition + 1 < hi)
+            quickSortUtil(arr, partition + 1, hi);
+    }
+
+    public static int partitionUtil(int[] arr, int lo, int hi) {
+        int pivot = lo;
+        lo++;
+        while (lo <= hi) {
+            while (lo <= hi && arr[pivot] >= arr[lo])
+                lo++;
+            while (arr[pivot] < arr[hi])
+                hi--;
+            if (hi > lo) {
+                swap(arr, lo, hi);
+            }
+        }
+        swap(arr, pivot, hi);
+        return hi;
+    }
